7c99000125c09eaa6e60bcf800a20c011cbe236d
JohnMorrisBerkleeOnline <41553512+JohnMorrisBerkleeOnline@users.noreply.github.com>
2018-07-22 20:53:24 -0500

    Add files via upload

---
+++ assingment4 part 2.js
@@ -0,0 +1,12 @@
+let my_song = [
+  {note: 'A', starts.at: 0, lasts:1}
+  {note: 'C', starts.at: 1, lasts:1}
+  {note: 'B', starts.at: 2, lasts:1}
+  {note: 'D', starts.at: 3, lasts:1}
+  {note: 'C', starts.at: 4, lasts:1}
+  {note: 'E', starts.at: 5, lasts:1}
+  {note: 'D', starts.at: 6, lasts:1}
+  {note: 'F', starts.at: 7, lasts:1}
+  {note: 'E', starts.at: 8, lasts:1}
+  {note: 'G', starts.at: 9, lasts:1}
+];
